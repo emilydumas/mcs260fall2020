dc715ab7b173d2d75ca9249c9791692def64fa71
David Dumas <david@dumas.io>
2020-11-28 14:48:54 -0600

    Updates from lecture 40

--- samplecode/tkinter/lasercode.py
+++ samplecode/tkinter/lasercode.py
@@ -14,37 +14,60 @@ class EncoderApp(tkinter.Tk):
         self.title("LaserCode 9 Series X+")
 
         self.input_text = tkinter.StringVar()
-        self.input_text.trace_add("write",lambda a,b,c:self.input_text_changed())
+        self.input_text.trace_add("write",lambda a,b,c:self.recalculate_output())
         self.output_text = tkinter.StringVar()
 
         self.input_label = tkinter.ttk.Label(self,text="Input:")
-        self.input_label.grid(row=0,column=0,sticky="w")
+        self.input_label.grid(row=0,column=0,sticky="w",padx=8,pady=4)
 
         self.output_label = tkinter.ttk.Label(self,text="Output:")
-        self.output_label.grid(row=1,column=0,sticky="w")
+        self.output_label.grid(row=1,column=0,sticky="w",padx=8,pady=4)
 
         self.input_entry = tkinter.ttk.Entry(self,width=50,textvariable=self.input_text)
-        self.input_entry.grid(row=0,column=1,columnspan=2,sticky="ew")
+        self.input_entry.grid(row=0,column=1,columnspan=2,sticky="ew",padx=8,pady=4)
 
-        self.output_entry = tkinter.ttk.Entry(self,width=50,textvariable=self.output_text)
-        self.output_entry.grid(row=1,column=1,columnspan=2,sticky="ew")
+        self.output_entry = tkinter.ttk.Entry(self,width=50,state="readonly",textvariable=self.output_text)
+        self.output_entry.grid(row=1,column=1,columnspan=2,sticky="ew",padx=8,pady=4)
 
         self.steps_label = tkinter.ttk.Label(self,text="Steps:")
-        self.steps_label.grid(row=2,column=0,sticky="w")
+        self.steps_label.grid(row=2,column=0,sticky="w",padx=8,pady=4)
 
-        self.steps_slider = tkinter.ttk.Scale(self)
-        self.steps_slider.grid(row=2,column=1,sticky="ew")
+        self.steps = tkinter.IntVar()
+        self.steps.trace_add("write",lambda a,b,c:self.recalculate_output())
+        self.steps.trace_add("write",lambda a,b,c:self.update_steps_indicator())
 
-        self.steps_indicator = tkinter.ttk.Label(self,text="99",width=2)
-        self.steps_indicator.grid(row=2,column=2,sticky="w")
+        self.steps_slider = tkinter.ttk.Scale(self,from_=0,to=25,variable=self.steps)
+        self.steps_slider.grid(row=2,column=1,sticky="ew",padx=8,pady=4)
 
-        self.reverse_check = tkinter.ttk.Checkbutton(self,text="Reverse string")
-        self.reverse_check.grid(row=3,column=1,sticky="w")
+        self.steps_indicator_content = tkinter.StringVar()
 
-    def input_text_changed(self):
+        self.steps_indicator = tkinter.ttk.Label(self,textvariable=self.steps_indicator_content,width=2)
+        self.steps_indicator.grid(row=2,column=2,sticky="w",padx=8,pady=4)
+
+        self.reverse_flag = tkinter.IntVar()
+        self.reverse_flag.trace_add("write",lambda a,b,c:self.recalculate_output())
+
+        self.reverse_check = tkinter.ttk.Checkbutton(self,text="Reverse string",variable=self.reverse_flag)
+        self.reverse_check.grid(row=3,column=1,sticky="w",padx=8,pady=4)
+
+        self.exit_button = tkinter.ttk.Button(self,text="Exit",command=exit)
+        self.exit_button.grid(row=3,column=2,sticky="e",padx=8,pady=4)
+
+        # Initialize the steps indicator to whatever the initial
+        # value of the step slider is.
+        self.update_steps_indicator()
+
+    def update_steps_indicator(self):
+        """Make steps indicator label consistent with the slider position"""
+        self.steps_indicator_content.set(str(self.steps.get()))
+
+    def recalculate_output(self):
         """callback for changes to input text entry box"""
         s = self.input_text.get()
-        senc = rotcode.rotate(s,13)
+        senc = rotcode.rotate(s,self.steps.get())
+        if self.reverse_flag.get():
+            # Reverse the encoded string
+            senc = senc[::-1]
         self.output_text.set(senc)
 
 app = EncoderApp()
